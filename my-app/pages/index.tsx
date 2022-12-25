6377d82eeaa75b83cb06c5b40e4f3efe699b3a4a
Tagi17 <naziaozair@gmail.com>
2022-12-25 13:38:31 -0500

    Update index.tsx

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import Web3Modal from "web3modal";
+import Web3Modal from "web3Modal";
 import { providers, Contract } from "ethers";
 import { useEffect, useRef, useState } from "react";
 import { WHITELIST_CONTRACT_ADDRESS, abi } from "../constants";
